99388ead51d62b7bfa92365694f1523ac292d0bc
cogle <thecogle@gmail.com>
2016-12-15 21:28:43 -0600

    Pushing updates

--- AtomicBufferQueue/AtomicBufferQueue.cpp
+++ AtomicBufferQueue/AtomicBufferQueue.cpp
@@ -15,7 +15,22 @@ int main(int argc, char * argv[])
 	/*
 	Example Usage here
 	*/
+	RingBuffer<int> r { 20 };
+	for (unsigned int i = 0; i < 10; i++) {
+		r.push(10);
+	}
+	int temp = 1;
+	for (unsigned int i = 0; i < 10; i++) {
+		r.push(temp);
+	}
 
+	for (int i = 0; i < 20; i++) {
+		std::shared_ptr<int>  val = r.pop();
+		if ( val == nullptr) { std::cout << "HERE" << std::endl; }
+		else {
+			std::cout << "Index: " << i << " element: " << *val << std::endl;
+		}
+	}
 	return 0;
 }
 
--- AtomicBufferQueue/RingBuffer.h
+++ AtomicBufferQueue/RingBuffer.h
@@ -7,7 +7,7 @@
 template <typename T> class RingBuffer {
 public:
 
-	explicit RingBuffer(std::size_t capacity) : _bufferCapacity(capacity), _ringBufferArray(new T[capacity]) {
+	explicit RingBuffer(std::size_t capacity) : _bufferCapacity(capacity), _ringBufferArray(new T[capacity + 1]) {
 
 	}
 
@@ -22,20 +22,21 @@ public:
 	the transaction was completed. If it was not able to complete then the
 	user can sleep/yield or perform other work in the calling thread.
 	*/
+	
 	template<typename U>
 	bool push(U && val)
 	{
 		return tryPush(std::forward<U>(val));
 	}
+	
 
 	/*
 	Returns a shared_ptr to the value that is popped off the thread. If there is nothing
 	in the queue or the queue is full then the user is returned a nullptr, and can proceed accordingly.
 	*/
 	std::shared_ptr<T> pop() {
-		std::shared_ptr<T> * val(nullptr);
-		tryPop(&val);
-		return val;
+		std::shared_ptr<T> ret  = tryPop();
+		return ret;
 	}
 
 	std::size_t maxCapacity() {
@@ -60,26 +61,26 @@ private:
 		const std::size_t currentRead = _readPosition.load(std::memory_order_acquire);
 		const std::size_t nextWrite = incrementIndex(currentWrite);
 
-		if (nextWrite == current_read) { return false; }
+		if (nextWrite == currentRead) { return false; }
 
 		_ringBufferArray[currentWrite] = std::move(val);
-		_writePosition.store(next_write, std::memory_order_release);
+		_writePosition.store(nextWrite, std::memory_order_release);
 
 		return true;
 	}
 
-	void tryPop(T *& val) {
+	std::shared_ptr<T> tryPop() {
+		std::shared_ptr<T> val(nullptr);
 		const std::size_t currentWrite = _writePosition.load(std::memory_order_acquire);
 		const std::size_t currentRead = _readPosition.load(std::memory_order_acquire);
 
-		if (currentRead == currentWrite) { return false; }
+		if (currentRead == currentWrite) { return val; }
 
 		val = std::make_shared<T>(_ringBufferArray[currentRead]);
 
 		const std::size_t nextRead = incrementIndex(currentRead);
 		_readPosition.store(nextRead, std::memory_order_release);
-
-		return true;
+		return val;
 	}
 
 	std::size_t incrementIndex(std::size_t index) {
